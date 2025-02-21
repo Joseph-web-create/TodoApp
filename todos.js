3b788c6d9c33772d1a6de3409e4b84ec37d7450b
Aace <aacejovi@gmail.com>
2025-02-21 12:35:43 +0100

    modal functionalit done

---
+++ todos.js
@@ -0,0 +1,13 @@
+const container = document.getElementById("container-div");
+const addBtn = document.getElementById("add-task");
+const modal = document.getElementById("modal");
+const closs = document.getElementById("close");
+const add1 = document.getElementById("add1");
+
+const todos = [];
+
+
+add1.addEventListener("click", () => {
+  modal.classList.remove("hidden");
+  modal.classList.add("flex");
+});
