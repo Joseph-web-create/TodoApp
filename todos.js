810d66cc4e5e3ef28160e626e72075e946a8a9a2
Aace <aacejovi@gmail.com>
2025-02-22 14:24:41 +0100

    Need to study

--- todos.js
+++ todos.js
@@ -7,36 +7,34 @@ const add1 = document.getElementById("add1");
 const titleInput = document.getElementById("titleinput");
 const dateInput = document.getElementById("dateinput");
 const textArea = document.getElementById("text-area");
-const deletedDiv = document.getElementById("deleted-div");
-const hddnBtn = document.getElementById("hidden-btn")
 
 const taskData = [];
-const deletedData = [];
+const deletedData1 = JSON.parse(localStorage.getItem("data")) || [];
 
-const addtask = () =>{
-        const date1 = new Date();
+const addtask = () => {
+  const date1 = new Date();
 
-        const taskObj = {
-          id: `${titleInput.value
-            .toLowerCase()
-            .split(" ")
-            .join("-")}-${date1.getTime()}
+  const taskObj = {
+    id: `${titleInput.value
+      .toLowerCase()
+      .split(" ")
+      .join("-")}-${date1.getTime()}
           `,
-          title: titleInput.value,
-          dueDate: dateInput.value,
-          textArea: textArea.value,
-          time: date1.getHours() + ":" + date1.getMinutes(),
-        };
+    title: titleInput.value,
+    dueDate: dateInput.value,
+    textArea: textArea.value,
+    time: date1.getHours() + ":" + date1.getMinutes(),
+  };
 
-        taskData.unshift(taskObj);
-        updatePage();
-        reset();
-}
+  taskData.unshift(taskObj);
+  updatePage();
+  reset();
+};
 
-const updatePage = () =>{
-        container.innerHTML = "";
-        taskData.forEach((task) =>{
-                container.innerHTML += `
+const updatePage = () => {
+  container.innerHTML = "";
+  taskData.forEach((task) => {
+    container.innerHTML += `
                 <div class="p-3 rounded-[5px] bg-[#eeeeee]" id="${task.id}">
                         <h2 class="text-[1.5rem] font-bold">${task.title}</h2>
                         <p class="text-[green] my-[10px]">Due date: ${task.dueDate}</p>
@@ -49,29 +47,28 @@ const updatePage = () =>{
                                 text-[white] cursor-pointer" onclick="deleteTaskBtn(this)">Delete</button>
                         </div>
                 </div>`;
-        })
+  });
 
-        modal.classList.remove("flex");
-        modal.classList.add("hidden");
-}
+  modal.classList.remove("flex");
+  modal.classList.add("hidden");
+};
 
-const deleteTaskBtn = (ele) =>{
-        const findId = taskData.findIndex(task => task.id === ele.parentElement.id)
-        deletedData.unshift(taskData.splice(findId, 1));
-        ele.parentElement.remove()
-        updatePage()
-        if(deletedData.length){
-                hddnBtn.classList.remove("hidden");
-        }
-}
+const deleteTaskBtn = (ele) => {
+  const findId = taskData.findIndex((task) => task.id === ele.parentElement.id);
+  deletedData1.unshift(taskData.splice(findId, 1));
+  ele.parentElement.remove();
+  updatePage();
+  if (deletedData1.length) {
+    hddnBtn.classList.remove("hidden");
+  }
+};
 
-console.log(deletedData);
 
-const reset = () =>{
-        titleInput.value = "";
-        dateInput.value = "";
-        textArea.value = "";
-}
+const reset = () => {
+  titleInput.value = "";
+  dateInput.value = "";
+  textArea.value = "";
+};
 
 addBtn.addEventListener("click", addtask);
 
@@ -80,10 +77,10 @@ add1.addEventListener("click", () => {
   modal.classList.add("flex");
 });
 
-closs.addEventListener("click", ()=>{
-        modal.classList.remove("flex");
-        modal.classList.add("hidden")
-})
+closs.addEventListener("click", () => {
+  modal.classList.remove("flex");
+  modal.classList.add("hidden");
+});
 
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
