4e0d4a89e5df0b35b8ef1a0c1e7ad5c3a50bd9da
Aace <aacejovi@gmail.com>
2025-03-01 12:03:08 +0100

    will be returning back to learn and update in the future

--- todos.js
+++ todos.js
@@ -13,11 +13,7 @@ const taskData = JSON.parse(localStorage.getItem("Mydata")) || [];
 const addtask = () => {
   const date1 = new Date();
 
-  if (
-    titleInput.value.trim() === "" ||
-    dateInput.value === "" ||
-    textArea.value.trim() === ""
-  ) {
+  if (titleInput.value.trim() === "") {
     alert(`Input field's are empty enter your input's`);
     modal.style.display = "none";
     return;
@@ -50,7 +46,7 @@ const updatePage = () => {
     <div class="p-3 rounded-[5px] bg-[#eeeeee] w-[100%]" id="${task.id}">
       <h2 class="text-[1.5rem] font-bold">${task.title}</h2>
       <p class="text-[green] my-[10px]">Due date: ${task.dueDate}</p>
-      <h2 class="font-bold mb-[5px]">Description</h2>
+      <h3 class="font-bold mb-[5px]">Description</h3>
       <p>${task.textArea}</p>
       <div class="flex justify-between items-center mt-[8px]">
         <h3 class="bg-[var(--bgheader)] p-2 rounded-[5px] text-[white]">Time: ${task.time}</h3>
